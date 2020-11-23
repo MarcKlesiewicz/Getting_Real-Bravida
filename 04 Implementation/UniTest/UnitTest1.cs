b8aae9680be5928fbdc514fafc56fb8bef0e6794
Rune Kennet Christensen <rkch26738@edu.ucl.dk>
2020-11-23 13:29:14 +0100

    Unitest med lister

--- 04 Implementation/UniTest/UnitTest1.cs
+++ 04 Implementation/UniTest/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace UniTest
             ab = new ArbejdsbeskrivelseRepo();
             cs.OpretAftaleseddel();
 
-            
+
         }
 
         [TestMethod]
@@ -29,6 +29,7 @@ namespace UniTest
 
             Assert.AreEqual("Danny er sej", cs.ValgtArbejdsbeskrivelse.Tekst);
         }
+
         [TestMethod]
         public void RedigerAntal()
         {
@@ -39,22 +40,24 @@ namespace UniTest
 
             Assert.AreEqual(50, cs.ValgtArbejdsbeskrivelse.Antal);
         }
+
         [TestMethod]
         public void RedigerEnhedsPris()
         {
             cs.OpretArbejdsbeskrivelse();
-            Assert.AreEqual( 0.0, cs.ValgtArbejdsbeskrivelse.EnhedsPris);
+            Assert.AreEqual(0.0, cs.ValgtArbejdsbeskrivelse.EnhedsPris);
 
             cs.RedigerArbejdsbeskrivelse(20.0);
 
             Assert.AreEqual(20.0, cs.ValgtArbejdsbeskrivelse.EnhedsPris);
         }
+
         [TestMethod]
         public void DeaktiverArbejdsbeskrivelse()
         {
             cs.OpretArbejdsbeskrivelse();
 
-            Assert.AreEqual(true, cs.ValgtArbejdsbeskrivelse.Aktiveret); 
+            Assert.AreEqual(true, cs.ValgtArbejdsbeskrivelse.Aktiveret);
 
             cs.DeaktiverArbejdsbeskrivelse();
 
@@ -62,5 +65,67 @@ namespace UniTest
 
         }
 
+        [TestClass]
+        public class UnitTestListe
+        {
+            Controller cs;
+            ArbejdsbeskrivelseRepo ab;
+
+            [TestInitialize]
+            public void Initialize()
+            {
+                cs = new Controller();
+                ab = new ArbejdsbeskrivelseRepo();
+                cs.OpretAftaleseddel();
+
+
+            }
+
+            [TestMethod]
+            public void RedigerBeskrivelse()
+            {
+                cs.OpretArbejdsbeskrivelse();
+                Assert.AreEqual(null, cs.ValgtArbejdsbeskrivelse.Tekst);
+
+                cs.RedigerArbejdsbeskrivelse("Danny er sej");
+
+                Assert.AreEqual("Danny er sej", cs.VisArbejdsbeskrivelser(0)[0].Tekst);
+            }
+
+            [TestMethod]
+            public void RedigerAntal()
+            {
+                cs.OpretArbejdsbeskrivelse();
+                Assert.AreEqual(0.0, cs.ValgtArbejdsbeskrivelse.Antal);
+
+                cs.RedigerArbejdsbeskrivelse(50);
+
+                Assert.AreEqual(50, cs.VisArbejdsbeskrivelser(0)[0].Antal);
+            }
+
+            [TestMethod]
+            public void RedigerEnhedsPris()
+            {
+                cs.OpretArbejdsbeskrivelse();
+                Assert.AreEqual(0.0, cs.ValgtArbejdsbeskrivelse.EnhedsPris);
+
+                cs.RedigerArbejdsbeskrivelse(20.0);
+
+                Assert.AreEqual(20.0, cs.VisArbejdsbeskrivelser(0)[0].EnhedsPris);
+            }
+
+            [TestMethod]
+            public void DeaktiverArbejdsbeskrivelse()
+            {
+                cs.OpretArbejdsbeskrivelse();
+
+                Assert.AreEqual(true, cs.ValgtArbejdsbeskrivelse.Aktiveret);
+
+                cs.DeaktiverArbejdsbeskrivelse();
+
+                Assert.AreEqual(false, cs.VisArbejdsbeskrivelser(0)[0].Aktiveret);
+
+            }
+        }
     }
 }
